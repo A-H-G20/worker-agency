8a7e71c7dbf2192fd5322da163f6bb9809ab6f25
Ahmad Ghosen <ahmadghosen20@gmail.com>
2024-09-15 22:43:47 +0300

    edit register page

--- signup.php
+++ signup.php
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $date_of_birth = $_POST['date_of_birth'];
     $phone_number = $_POST['phone_number'];
     $password = $_POST['password'];
-
+    $role ='user';
     $mail = new PHPMailer(true);
 
     try {
@@ -58,7 +58,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 
                 // Insert the new user into the database
-                $stmt = $conn->prepare("INSERT INTO users (name, username, email, gender, category, address, date_of_birth, phone_number, password, verification_code) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                $stmt = $conn->prepare("INSERT INTO users (name, username, email, gender, category, address, date_of_birth, phone_number, password, verification_code,role) VALUES (?, ?, ?, ?,?, ?, ?, ?, ?, ?, 'user')");
                 $verification_code = substr(number_format(time() * rand(), 0, '', ''), 0, 6);
                 $stmt->bind_param("ssssssssss", $name, $username, $email, $gender, $category, $address, $date_of_birth, $phone_number, $hashed_password, $verification_code);
 
@@ -112,7 +112,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <form method="POST" action="">
             <div class="form-container">
                 <div class="right-side">
-                    <label for="name">Name:</label>
+                    <label for="name">Full Name:</label>
                     <input type="text" id="name" name="name" required>
 
                     <label for="email">Email:</label>
@@ -121,10 +121,113 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <label for="category">Category:</label>
                     <select id="category" name="category" required>
                         <option value="">Select Category</option>
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-                        <option value="Other">Other</option>
+                        <option value="Accounting & Auditing">Accounting & Auditing</option>
+                        <option value="Computer Science">Computer Science</option>
+                        <option value="Acting">Acting</option>
+                        <option value="Administrative Roles">Administrative Roles</option>
+                        <option value="Advertising">Advertising</option>
+                        <option value="Aerospace & Aeronautics">Aerospace & Aeronautics</option>
+                        <option value="Air Traffic Control">Air Traffic Control</option>
+                        <option value="Architecture">Architecture</option>
+                        <option value="Banking & Finance">Banking & Finance</option>
+                        <option value="Bartenders">Bartenders</option>
+                        <option value="Beauty Industry">Beauty Industry</option>
+                        <option value="Biologists">Biologists</option>
+                        <option value="Biotechnology">Biotechnology</option>
+                        <option value="Botanists">Botanists</option>
+                        <option value="Business & Operations Management">Business & Operations Management</option>
+                        <option value="Business Analysts">Business Analysts</option>
+                        <option value="Business Development">Business Development</option>
+                        <option value="Carpentry">Carpentry</option>
+                        <option value="Chef & Culinary Arts">Chef & Culinary Arts</option>
+                        <option value="Chemical Industry">Chemical Industry</option>
+                        <option value="Childcare">Childcare</option>
+                        <option value="Commercial Artist">Commercial Artist</option>
+                        <option value="Community Services">Community Services</option>
+                        <option value="Compliance & Regulatory Affairs">Compliance & Regulatory Affairs</option>
+                        <option value="Construction Industry">Construction Industry</option>
+                        <option value="Consulting">Consulting</option>
+                        <option value="Counseling">Counseling</option>
+                        <option value="Crafts">Crafts</option>
+                        <option value="Cybersecurity">Cybersecurity</option>
+                        <option value="Dentistry">Dentistry</option>
+                        <option value="Diplomacy">Diplomacy</option>
+                        <option value="Electrician">Electrician</option>
+                        <option value="Engineering">Engineering</option>
+                        <option value="Event Planning">Event Planning</option>
+                        <option value="Fashion Industry">Fashion Industry</option>
+                        <option value="Fine Art">Fine Art</option>
+                        <option value="Fitness & Wellness">Fitness & Wellness</option>
+                        <option value="Forestry">Forestry</option>
+                        <option value="Government Worker">Government Worker</option>
+                        <option value="Hotel Industry">Hotel Industry</option>
+                        <option value="Insurance Industry">Insurance Industry</option>
+                        <option value="Customer Service">Customer Service</option>
+                        <option value="Data Science">Data Science</option>
+                        <option value="Design">Design</option>
+                        <option value="Education & Training">Education & Training</option>
+                        <option value="Emergency Services">Emergency Services</option>
+                        <option value="Environmental Science">Environmental Science</option>
+                        <option value="Farming & Agricultural Science">Farming & Agricultural Science</option>
+                        <option value="Film Industry">Film Industry</option>
+                        <option value="Fishing">Fishing</option>
+                        <option value="Flight Attendants">Flight Attendants</option>
+                        <option value="Game Developers">Game Developers</option>
+                        <option value="Hospitality Industry">Hospitality Industry</option>
+                        <option value="Human Resources">Human Resources</option>
+                        <option value="Investment Banking">Investment Banking</option>
+                        <option value="Journalism">Journalism</option>
+                        <option value="Landscaping">Landscaping</option>
+                        <option value="Librarians">Librarians</option>
+                        <option value="Manufacturing Industry">Manufacturing Industry</option>
+                        <option value="Marketing">Marketing</option>
+                        <option value="Mathematicians">Mathematicians</option>
+                        <option value="Medicine">Medicine</option>
+                        <option value="Culture Industry">Culture Industry</option>
+                        <option value="Nonprofit Management">Nonprofit Management</option>
+                        <option value="Occupational Health & Safety">Occupational Health & Safety</option>
+                        <option value="Personal Services">Personal Services</option>
+                        <option value="Pharmaceutical Industry">Pharmaceutical Industry</option>
+                        <option value="Pilots">Pilots</option>
+                        <option value="Laboratory Technicians">Laboratory Technicians</option>
+                        <option value="Law">Law</option>
+                        <option value="Maintenance & Repair Technicians">Maintenance & Repair Technicians</option>
+                        <option value="Market Research">Market Research</option>
+                        <option value="Materials Science">Materials Science</option>
+                        <option value="Media Industry">Media Industry</option>
+                        <option value="Museum Curator">Museum Curator</option>
+                        <option value="Music Industry">Music Industry</option>
+                        <option value="Nursing">Nursing</option>
+                        <option value="Office Worker">Office Worker</option>
+                        <option value="Pet Services & Animal Care">Pet Services & Animal Care</option>
+                        <option value="Physicists">Physicists</option>
+                        <option value="Professional Services">Professional Services</option>
+                        <option value="project_management">Project management</option>
+                        <option value="psychiatry">Psychiatry</option>
+                        <option value="public_relations">Public relations & communications</option>
+                        <option value="quality_assurance">Quality assurance</option>
+                        <option value="real_estate_agents">Real estate agents</option>
+                        <option value="research">Research</option>
+                        <option value="restaurant_industry">Restaurant industry</option>
+                        <option value="retail_industry">Retail industry</option>
+                        <option value="risk_management">Risk management</option>
+                        <option value="sales">Sales</option>
+                        <option value="science">Science</option>
+                        <option value="social_work">Social work</option>
+                        <option value="software_developer">Software developer</option>
+                        <option value="sports_industry">Sports industry</option>
+                        <option value="teaching">Teaching</option>
+                        <option value="technology_r&d">Technology research & development</option>
+                        <option value="technology_specialists">Technology specialists</option>
+                        <option value="technology_operations">Technology operations</option>
+                        <option value="trades">Trades</option>
+                        <option value="transportation_logistics">Transportation & logistics</option>
+                        <option value="travel_industry">Travel industry</option>
+                        <option value="veterinarians">Veterinarians</option>
+                        <option value="wildlife_conservation">Wildlife conservation</option>
+                        <option value="zoologists">Zoologists</option>
                     </select>
+
 
                     <label for="gender">Gender:</label>
                     <select id="gender" name="gender" required>
